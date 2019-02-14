9489e88a445c67cd4717b75c80232d46e62a378f
mohammad-babaei <35062696+mohammad-babaei@users.noreply.github.com>
2019-02-14 14:31:42 +0330

    fix bugs and add bfs

--- Classes.py
+++ Classes.py
@@ -1,11 +1,13 @@
 #from ks.models import (World, Police, Terrorist, Bomb, Position, Constants,
  #                      ESoundIntensity, ECell, EAgentStatus)
 from ks.models import ECell
+import collections
 class Node:
 	def __init__(self,x,y):                                          # ___x   (x,y) => cordinates
 		self.cordinates = (x,y)                                      #|
-		self.neighbors = []                                          #|
-                                                                     #y
+		self.neighbors = []
+		self.type = ""   # type of the node itself                                          #|
+																	 #y
 	#def _init_neighbors(self):
 class Map:
 	def __init__(self,board,width,height): # board , width , height are the game initiate attributes in decide function.
@@ -17,6 +19,7 @@ class Map:
 		self.LargeBombSites = []
 		self.MediumBombSites = []
 		self.SmallBombSites = []
+		self.graph = {}
 		self._init_map()
 
 
@@ -26,14 +29,15 @@ class Map:
 			for j in range(self.width):
 				node = Node(j,i)
 				self.Nodes[i].append(node)
-				_init_neighbors(node)
-				if(self.board[i][j] == LargeBombSite):
+				self._init_neighbors(node)
+				self._init_graph(node)
+				if(self.board[i][j] == ECell.LargeBombSite):
 					self.LargeBombSites.append((i,j))
-				elif(self.board[i][j] == VastBombSite):
+				elif(self.board[i][j] == ECell.VastBombSite):
 					self.VastBombSites.append((i,j))
-				elif(self.board[i][j] == SmallBombSite):
+				elif(self.board[i][j] == ECell.SmallBombSite):
 					self.SmallBombSites.append((i,j))
-				elif(self.board[i][j] == MediumBombSite):
+				elif(self.board[i][j] == ECell.MediumBombSite):
 					self.MediumBombSites.append((i,j))
 					
 	
@@ -53,3 +57,54 @@ class Map:
 				self.Nodes[i][j].neighbors.append(self.Nodes[i][j-1])
 		except:
 			pass
+
+
+	def _init_graph(self,node):
+
+		self.graph[node] = node.neighbors
+
+
+	def GetNodeByPosition(self,position):
+
+		return self.Nodes[position[0]][position[1]]
+
+
+
+
+
+
+
+class BFS:
+
+	def DoBfs(self,map,root,goal):
+
+		self.BreadthFirstSearch(root,map.graph,goal)
+
+	def BreadthFirstSearch(self,root,graph,goal):
+
+		parent = {}
+		parent[root] = root
+
+		queue = collections.deque([root])
+		while queue: 
+			vertex = queue.popleft()
+			for neighbour in graph[vertex]: 
+
+				if neighbour == goal:
+					parent[neighbour] = vertex
+					self.print_path(parent, neighbour, root)
+					return
+
+				if neighbour not in parent: 
+					parent[neighbour] = vertex
+					queue.append(neighbour)
+		print(" No path found ")
+
+	def print_path(self,parent, goal, start):
+		path = [goal]
+		# trace the path back till we reach start
+		while goal != start:
+			goal = parent[goal]
+			path.insert(0, goal)
+		print(path)
+
