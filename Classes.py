b61b2511de3db221b61797092f7a795946d2fe82
MohammadBabaei <mr.linux3@yahoo.com>
2019-02-14 22:43:28 +0330

    ai changed for testing and in Classes map.graph needs to be fixed

--- Classes.py
+++ Classes.py
@@ -1,110 +1,126 @@
-#from ks.models import (World, Police, Terrorist, Bomb, Position, Constants,
- #                      ESoundIntensity, ECell, EAgentStatus)
+# from ks.models import (World, Police, Terrorist, Bomb, Position, Constants,
+#                      ESoundIntensity, ECell, EAgentStatus)
 from ks.models import ECell
 import collections
+
+
 class Node:
-	def __init__(self,x,y):                                          # ___x   (x,y) => cordinates
-		self.cordinates = (x,y)                                      #|
-		self.neighbors = []
-		self.type = ""   # type of the node itself                                          #|
-																	 #y
-	#def _init_neighbors(self):
+    def __init__(self, x, y):  # ___x   (x,y) => cordinates
+        self.cordinates = (x, y)  # |
+        self.neighbors = []
+        self.type = ""  # type of the node itself                                          #|
+
+    # y
+
+    # def _init_neighbors(self):
+
+
 class Map:
-	def __init__(self,board,width,height): # board , width , height are the game initiate attributes in decide function.
-		self.board = board
-		self.Nodes = [] #this is the board with nodes.
-		self.width = width
-		self.height = height
-		self.VastBombSites = []
-		self.LargeBombSites = []
-		self.MediumBombSites = []
-		self.SmallBombSites = []
-		self.graph = {}
-		self._init_map()
+    def __init__(self, board, width,
+                 height):  # board , width , height are the game initiate attributes in decide function.
+        self.board = board
+        self.Nodes = []  # this is the board with nodes.
+        self.width = width
+        self.height = height
+        self.VastBombSites = []
+        self.LargeBombSites = []
+        self.MediumBombSites = []
+        self.SmallBombSites = []
+        self.graph = {}
+        self.graph2 = {}
+        self._init_map()
 
+    def _init_map(self):  # to initiate Map you must use this;now it used in __init__
+        for i in range(self.height):
+            self.Nodes.append([])
+            for j in range(self.width):
+                node = Node(j, i)
+                self.Nodes[i].append(node)
 
-	def _init_map(self):              # to initiate Map you must use this;now it used in __init__
-		for i in range(self.height):
-			self.Nodes.append([])
-			for j in range(self.width):
-				node = Node(j,i)
-				self.Nodes[i].append(node)
-				self._init_neighbors(node)
-				self._init_graph(node)
-				if(self.board[i][j] == ECell.LargeBombSite):
-					self.LargeBombSites.append((i,j))
-				elif(self.board[i][j] == ECell.VastBombSite):
-					self.VastBombSites.append((i,j))
-				elif(self.board[i][j] == ECell.SmallBombSite):
-					self.SmallBombSites.append((i,j))
-				elif(self.board[i][j] == ECell.MediumBombSite):
-					self.MediumBombSites.append((i,j))
-					
-	
+                self._init_neighbors(node)
 
-
-	def _init_neighbors(self,node):    #this function will be called in _init_map function.s
-		i = node.cordinates[1]
-		j = node.cordinates[0]		
-		try:
-			if(self.board[i+1][j] == Empty):
-				self.Nodes[i][j].neighbors.append(self.Nodes[i+1][j])
-			if(self.board[i-1][j] == Empty):
-				self.Nodes[i][j].neighbors.append(self.Nodes[i-1][j])
-			if(self.board[i][j+1] == Empty):
-				self.Nodes[i][j].neighbors.append(self.Nodes[i][j+1])
-			if(self.board[i][j-1] == Empty):
-				self.Nodes[i][j].neighbors.append(self.Nodes[i][j-1])
-		except:
-			pass
-
-
-	def _init_graph(self,node):
-
-		self.graph[node] = node.neighbors
-
-
-	def GetNodeByPosition(self,position):
-
-		return self.Nodes[position[0]][position[1]]
+                self.init_graph(node)
 
 
 
+                if (self.board[i][j] == ECell.LargeBombSite):
+                    self.LargeBombSites.append((i, j))
+                elif (self.board[i][j] == ECell.VastBombSite):
+                    self.VastBombSites.append((i, j))
+                elif (self.board[i][j] == ECell.SmallBombSite):
+                    self.SmallBombSites.append((i, j))
+                elif (self.board[i][j] == ECell.MediumBombSite):
+                    self.MediumBombSites.append((i, j))
+
+    def _init_neighbors(self, node):  # this function will be called in _init_map function.s
+        i = node.cordinates[1]
+        j = node.cordinates[0]
+
+        try:
+
+            if (self.board[i + 1][j] == ECell.Empty):
+                self.Nodes[i][j].neighbors.append(self.Nodes[i + 1][j])
+            if (self.board[i - 1][j] == ECell.Empty):
+                self.Nodes[i][j].neighbors.append(self.Nodes[i - 1][j])
+            if (self.board[i][j + 1] == ECell.Empty):
+                self.Nodes[i][j].neighbors.append(self.Nodes[i][j + 1])
+            if (self.board[i][j - 1] == ECell.Empty):
+                self.Nodes[i][j].neighbors.append(self.Nodes[i][j - 1])
+        except:
+            pass
 
 
+
+    def init_graph(self,node):
+
+        i = node.cordinates[1]
+        j = node.cordinates[0]
+
+        self.graph[self.Nodes[i][j]] = self.Nodes[i][j].neighbors
+
+        self.graph2[(self.Nodes[i][j].cordinates[0],self.Nodes[i][j].cordinates[1])] = [(boom.cordinates[0],boom.cordinates[1]) for boom in self.Nodes[i][j].neighbors]
+
+
+    def GetNodeByPosition(self, position):
+
+        return self.Nodes[position[0]][position[1]]
 
 
 class BFS:
+    def DoBfs(self, map, root, goal):
 
-	def DoBfs(self,map,root,goal):
+        self.BreadthFirstSearch(root, map.graph, goal,map.graph2)
 
-		self.BreadthFirstSearch(root,map.graph,goal)
+    def BreadthFirstSearch(self, root, graph, goal,graph2):
 
-	def BreadthFirstSearch(self,root,graph,goal):
+        parent = {}
+        parent[root] = root
 
-		parent = {}
-		parent[root] = root
+        print(graph2)
 
-		queue = collections.deque([root])
-		while queue: 
-			vertex = queue.popleft()
-			for neighbour in graph[vertex]: 
+        queue = collections.deque([root])
+        while queue:
+            vertex = queue.popleft()
+            i=0
+            for neighbour in graph[vertex]:
+                print(i)
+                i += 1
 
-				if neighbour == goal:
-					parent[neighbour] = vertex
-					self.print_path(parent, neighbour, root)
-					return
+                if neighbour == goal:
+                    parent[neighbour] = vertex
+                    self.print_path(parent, neighbour, root)
+                    return
 
-				if neighbour not in parent: 
-					parent[neighbour] = vertex
-					queue.append(neighbour)
-		print(" No path found ")
+                if neighbour not in parent:
+                    print("hi")
+                    parent[neighbour] = vertex
+                    queue.append(neighbour)
+        print(" No path found ")
 
-	def print_path(self,parent, goal, start):
-		path = [goal]
-		# trace the path back till we reach start
-		while goal != start:
-			goal = parent[goal]
-			path.insert(0, goal)
-		print(path)
-
+    def print_path(self, parent, goal, start):
+        path = [goal]
+        # trace the path back till we reach start
+        while goal != start:
+            goal = parent[goal]
+            path.insert(0, goal)
+        print(path)
